ac230495478c5faaaa4bd77bd91f2a15275ea735
Thomas Dunlap <ttdunlap33@gmail.com>
2021-05-20 20:49:27 -0600

    controllers index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const foodRoutes = require('./foodRoutes');
+const indoorRoutes = require('./indoorRoutes');
+const outdoorRoutes = require('./indoorRoutes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/food', foodRoutes);
+router.use('/indoor', indoorRoutes);
+router.use('/outdoor', outdoorRoutes);
+router.use('/home', homeRoutes);
 
 module.exports = router;
