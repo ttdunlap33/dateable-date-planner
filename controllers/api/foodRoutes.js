4179266188c9ca371f51bc340abbbc73566ee078
Thomas Dunlap <ttdunlap33@gmail.com>
2021-05-20 21:05:31 -0600

    working on foodroutes

--- controllers/api/foodRoutes.js
+++ controllers/api/foodRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Food } = require('../../models/Food');
 
 router.post('/', async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newFood = await Food.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newFood);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -16,22 +16,43 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const projectData = await Food.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!foodData) {
+      res.status(404).json({ message: 'No restaurant found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(foodData);
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get('/:id', (req, res) => {
+  Product.findOne({
+    include:[{
+      model: Food,
+      through: food_id,
+      as: "Restaurants"
+    }],
+    where: {
+      id: req.params.id
+    }
+  }).then(foodData => {
+    if (!foodData) {
+      res.status(404).json({message:"Could not find a restaurant with that id."})
+    }
+    res.json(foodData)}).catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  // find a single restaurant by its `id`
 });
 
 module.exports = router;
