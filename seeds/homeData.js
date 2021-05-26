9a8b049a2b241698037c3d18d2f3fd19fe19ee1f
zgibbs97 <zachary.gibbs15@gmail.com>
2021-05-26 13:04:03 -0700

    Creating Handlebars

--- seeds/homeData.js
+++ seeds/homeData.js
@@ -3,51 +3,50 @@ const { Home } = require('../models');
 const homeData = [
     {
         title: "Puzzles",
-        id: home_id,
+
     },
     {
         title: "Board Games",
-        id: home_id,
+
     },
     {
         title: "Cook a meal",
-        id: home_id,
+
     },
     {
         title: "Netflix & Chill",
-        id: home_id,
+
     },
     {
         title: "Make a Cereal Treat",
-        id: home_id,
+
     },
     {
         title: "Bake Cookies",
-        id: home_id,
     },
     {
         title: "Video Games",
-        id: home_id,
+
     },
     {
         title: "Stream an Online Class",
-        id: home_id,
+
     },
     {
         title: "Fondue Night",
-        id: home_id,
+
     },
     {
         title: "At Home Wine Tasting",
-        id: home_id,
+
     },
     {
         title: "Movie Marathon",
-        id: home_id,
+
     },
     {
         title: "Hire professionals for couples massage",
-        id: home_id,
+
     },
 ]
 
