100cc03e9f0fc5c1a218b22062c5e0b70d8fb137
Kevin Hanson <ziffoit@gmail.com>
2021-05-28 11:46:08 -0600

    random function food

--- seeds/foodData.js
+++ seeds/foodData.js
@@ -1,68 +1,67 @@
 const { Food } = require('../models');
 
 const foodData = [
-    {
-        title: "Watercourse",
-        cost: "$$",
-    },
-    {
-        title: "Tropical Smoothie",
-        cost: "$$",
-    },
-    {
-        title: "Woody's Woodfired Pizza",
-        cost: "$$",
-    },
-    {
-        title: "West on 29th",
-        cost: "$$$$",
-    },
-    {
-        title: "Himchuli",
-        cost: "$$$",
-    },
-    {
-        title: "City O' City",
-        cost: "$$",
-    },
-    {
-        title: "Lucile's Creole Cafe",
-        cost: "$$",
-    },
-    {
-        title: "Sassafras",
-        cost: "$$",
-    },
-    {
-        title: "Blue Pan",
-        cost: "$$",
-    },
-    {
-        title: "Work & Class",
-        cost: "$$",
-    },
-    {
-        title: "Sushi Sasa",
-        cost: "$$$",
-    },
-    {
-        title: "Los Chingones",
-        cost: "$$",
-    },
-    {
-        title: "Sol",
-        cost: "$$",
-    },
-    {
-        title: "Little Ollies",
-        cost: "$$",
-    },
-    {
-        title: "Happa",
-        cost: "$$",
-    },
-
-]
+  {
+    title: 'Watercourse',
+    cost: '$$',
+  },
+  {
+    title: 'Tropical Smoothie',
+    cost: '$$',
+  },
+  {
+    title: "Woody's Woodfired Pizza",
+    cost: '$$',
+  },
+  {
+    title: 'West on 29th',
+    cost: '$$$$',
+  },
+  {
+    title: 'Himchuli',
+    cost: '$$$',
+  },
+  {
+    title: "City O' City",
+    cost: '$$',
+  },
+  {
+    title: "Lucile's Creole Cafe",
+    cost: '$$',
+  },
+  {
+    title: 'Sassafras',
+    cost: '$$',
+  },
+  {
+    title: 'Blue Pan',
+    cost: '$$',
+  },
+  {
+    title: 'Work & Class',
+    cost: '$$',
+  },
+  {
+    title: 'Sushi Sasa',
+    cost: '$$$',
+  },
+  {
+    title: 'Los Chingones',
+    cost: '$$',
+  },
+  {
+    title: 'Sol',
+    cost: '$$',
+  },
+  {
+    title: 'Little Ollies',
+    cost: '$$',
+  },
+  {
+    title: 'Happa',
+    cost: '$$',
+  },
+];
 
 const seedFood = () => Food.bulkCreate(foodData);
 
